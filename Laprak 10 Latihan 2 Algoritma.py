54c75dc8e718f067c39fa593d6228f21063dccba
Elitawf <113889498+Elitawf@users.noreply.github.com>
2022-12-02 10:55:14 +0700

    Add files via upload

---
+++ Laprak 10 Latihan 2 Algoritma.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 10:32:32 2022
+
+@author: acer
+"""
+
+def tukar(A, i, j):
+    temp = A[i]
+    A[i] = A[j]
+    A[j] = temp
+
+def bubbleSort(A, n):
+    for i in range(n-1):
+        if A[i] > A[i + 1]:
+            tukar(A, i, i + 1)
+    if n - 1 > 1:
+        bubbleSort(A, n - 1)
+
+A = []
+bubbleSort(A, len(A))
+print("")
+print(A)
